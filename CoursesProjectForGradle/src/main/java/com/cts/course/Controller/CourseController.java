85a88c55de1f27e2ba2d21b946b1e6358e1aa0ea
882879 <882879@LTIN290739.cts.com>
2021-04-15 11:35:59 +0530

    Third Commit

--- CoursesProjectForGradle/src/main/java/com/cts/course/Controller/CourseController.java
+++ CoursesProjectForGradle/src/main/java/com/cts/course/Controller/CourseController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.cts.course.model.Courses;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CourseController {
 
 	@Autowired
